059d4cefd2fbaf44d5d9fe96b2a481141ad838e3
Lepennetier <GildasLepennetier@users.noreply.github.com>
2024-02-13 09:11:37 +0100

    make function better

--- functions.R
+++ functions.R
@@ -3,14 +3,17 @@ library(tidyverse)
 library(robotstxt)
 library(rvest)
 
+print("loading function: setup_scraping_env()")
 setup_scraping_env <- function(ROOT_LINK) {
   ROBOTS <- robotstxt(ROOT_LINK)
   SESSION <- session(ROOT_LINK)
   if (!ROBOTS$check(ROOT_LINK)) {stop(paste0("robots.txt does not allow scraping: ", ROOT_LINK))}
   SESSION <- session_jump_to(SESSION, ROOT_LINK)
   page <- read_html(SESSION)
+  return(list(SESSION = SESSION, page = page, ROBOTS = ROBOTS))
 }
 
+print("loading function: get_all_links()")
 get_all_links <- function(page) {
 	elements <- html_elements(page, "a")
 	texts <- tibble(text = html_text(elements))
